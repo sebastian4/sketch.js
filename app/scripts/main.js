d0019d90b2c7bc4f3f6e034899b702f5ef51ceae
sebastian <sebastian.lopezx@yahoo.com>
2013-12-24 09:10:06 -0500

    added increase decrease grid, need to fix the drawing when snap to grid

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,14 @@
-console.log('\'Allo \'Allo!');
+console.log("starting");
 
 $(function() {
-	
-    $.each(['#f00', '#ff0', '#0f0', '#0ff', '#00f', '#000', '#fff'], function() {
-      $('#tools').append("<a href='#colored_sketch' data-color='" + this + "' style='border: 1px solid black; width: 30px; height: 30px; background: " + this + "; display: inline-block;'></a> ");
-    });
 
-    $('#colored_sketch').sketch();
-    
-    ////
-    
-    $('#simple_sketch').sketch();
+  $.each(['#f00', '#ff0', '#0f0', '#0ff', '#00f', '#f0f', '#000', '#fff'], function() {
+    $('#colors_demo').append("<a href='#colors_sketch' data-color='" + this + "' style='width: 10px; background: " + this + ";'>__</a> ");
+  });
+  $.each([3, 5, 10, 15], function() {
+    $('#colors_demo').append("<a href='#colors_sketch' data-size='" + this + "' style='background: #ccc'>" + this + "</a> ");
+  });
+  $('#colors_sketch').sketch();
+  
+  $('#simple_sketch').sketch();
 });
