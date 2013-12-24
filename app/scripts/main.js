863ca5737de02c75ad8fd3eef68df81bd187e622
sebastian <sebastian.lopezx@yahoo.com>
2013-12-24 08:22:03 -0500

    took out the originals and added to the base

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,14 @@
 console.log('\'Allo \'Allo!');
+
+$(function() {
+	
+    $.each(['#f00', '#ff0', '#0f0', '#0ff', '#00f', '#000', '#fff'], function() {
+      $('#tools').append("<a href='#colored_sketch' data-color='" + this + "' style='border: 1px solid black; width: 30px; height: 30px; background: " + this + "; display: inline-block;'></a> ");
+    });
+
+    $('#colored_sketch').sketch();
+    
+    ////
+    
+    $('#simple_sketch').sketch();
+});
